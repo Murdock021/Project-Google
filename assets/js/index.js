7b398920c3da5c6a83bbf62ff986d7f4a36d9714
Murdock021 <diogomachadocmb@gmail.com>
2024-01-19 08:43:37 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const searchForm = document.querySelector('#search-form');
+    
+    searchForm.addEventListener('submit', (event) => {
+
+        const searchField = document.querySelector('#search-field').value;    
+        
+        if (event.submitter.name === 'btnK' && searchField.trim() === ''){
+            event.preventDefault(); 
+        }
+
+    });
+    
+});
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
